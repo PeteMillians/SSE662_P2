0aee61a929cf96e5b2620c529ebc71b129395581
millians_pm <11026416@live.mercer.edu>
2026-01-24 19:54:56 -0500

    Test the calculator add function

--- tests/run_tests.f90
+++ tests/run_tests.f90
@@ -3,6 +3,7 @@ program run_tests
     use test_utility
     implicit none
 
+    ! Utility tests
     call test_split()
     call test_getNumbersPositiveImag_format0()
     call test_getNumbersNegativeImag_format0()
@@ -12,7 +13,10 @@ program run_tests
     call test_getNumbersNegativeImag_format1()
     call test_getNumbersPositiveReal_format1()
     call test_getNumbersNegativeReal_format1()
+
+    ! ! Interface Tests
     call test_validate()
+    call test_add()
 
     print *, "All tests passed."
 end program run_tests
--- tests/test_interface.f90
+++ tests/test_interface.f90
@@ -57,4 +57,35 @@ contains
         end if
 
     end subroutine test_validate
+
+    !--------------------!
+    !   Calculate Tests  !
+    !--------------------!
+    subroutine test_add()
+
+        character(len = 256) :: input
+        character(len = 256) :: answer
+
+        input = "add 4-2i 4+3i"
+
+        answer = Calculate(input)
+
+        if (answer /= "(8.0000, 1.0000)") then
+            print *, "FAIL test_add: ", answer 
+            stop 1
+
+        end if
+
+        input = "add (3,2) (-3,70.5)"
+
+        answer = Calculate(input)
+
+        if (answer /= "(.0000, 72.5000)") then
+            print *, "FAIL test_add: ", answer 
+            stop 1
+
+        end if
+
+    end subroutine test_add
+
 end module test_interface
